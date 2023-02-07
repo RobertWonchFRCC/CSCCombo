105099cd71e552fff7c7944c60d10ca387ab871a
Wonch <Robert.Wonch@frontrange.edu>
2023-02-07 12:04:12 -0700

    HelloWorld

---
+++ HelloCSCCombo/src/HelloCSCCombo.java
@@ -0,0 +1,20 @@
+/***************************************************
+ * This is an app that will display 
+ * "Hello CSC Combo Class"
+ * 
+ * Due:2/7/2023
+ * Bob Wonch
+ * CSCCombo class
+ * 
+ ***************************************************/
+public class HelloCSCCombo {
+
+	public static void main(String[] args) {
+		
+		System.out.print("************************\n"); //standard print without a newline the\n creates the carriage return
+		System.out.println("*Hello CSC Combo Class!*");//standard println adds a newline at the end automagically
+		System.out.println("************************\n");//a println with a \n create double spaced row		
+		
+	}
+
+}
